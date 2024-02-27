94ba2cc7a6b135f7b04d3efaa425da2d65c26146
Fabián E. García <fabianegl79@gmail.com>
2024-02-27 11:25:58 -0300

    agregado de powerup, etiquetas, triggers, rigidbody y script de moviemiento en enemigos

---
+++ Assets/Scripts/MoveToPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    public float speed = 0.5f;
+
+    private Rigidbody _objectRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        _objectRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindPlayer();
+    }
+
+    void FindPlayer ()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        _objectRb.AddForce(lookDirection * speed);
+    }
+}
